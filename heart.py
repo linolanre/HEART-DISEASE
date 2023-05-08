cfcdfe3d0017cb828aed4898cde79901255556eb
linolanre <126574611+linolanre@users.noreply.github.com>
2023-05-08 18:29:10 +0100

    Update heart.py

--- heart.py
+++ heart.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import warnings 
 warnings.filterwarnings('ignore')
-from sklearn.linear_model import LogisticRegression
 import joblib
 
 model = joblib.load(open('LinReg.pkl', 'rb'))
